119e7a0a86aa7ef1b132fbf207c366d7fc0d734b
Amos Rose <amosroseacct@gmail.com>
2020-07-10 09:40:31 -0600

    ex3.py complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,23 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # initialize cache
+    cache = {}
+    # initialize result
+    result = []
+    # iterate through arrays with index
+    for i in range(len(arrays)):
+        # iterate through array in arrays
+        for item in arrays[i]:
+            # if item not in cache add item to cache with [index] as value
+            if item not in cache:
+                cache[item] = [i]
+            # if item in cache append index to cache[item]
+            else:
+                cache[item].append(i)
+    # iterate through cache
+    for key in cache:
+        # if length of cache[key] == length of arrays append to solution
+        if len(cache[key]) == len(arrays):
+            result.append(key)
 
     return result
 
